66836eb713bd6bf11f7289aad0c31193015abd3e
Salman2459 <muhammadsalman2160@gmail.com>
2025-07-28 14:05:00 +0500

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
   output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
 }
 
-export default nextConfig
+module.exports = nextConfig
