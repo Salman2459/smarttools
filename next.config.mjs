dc666bd64ddd9c1389f5acdb3adc086bee2ebe0d
Salman2459 <muhammadsalman2160@gmail.com>
2025-08-03 12:58:14 +0500

    ....

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   trailingSlash: true, // optional, depends on your routing preference
   images: {
     unoptimized: true, // only if you’re not using Image Optimization
   },
-  assetPrefix: 'https://smarttools.fun/',
+  // assetPrefix: 'https://smarttools.fun/',
 };
 
 export default nextConfig;
